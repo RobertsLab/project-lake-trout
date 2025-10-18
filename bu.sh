e0b6546da4f7bbd479bda04e28e4b2b6d60d9d65
STEVEN <sr320@uw.edu>
2025-10-18 06:21:36 -0700

    05 align

---
+++ bu.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# rsync -avz -e ssh . \
+# --exclude='*.sam' \
+# --exclude='tmp*' \
+# --exclude='*gz_C_to_T.fastq' \
+# --exclude='*gz_G_to_A.fastq' \
+# --exclude='Non_CpG_context*' \
+# --exclude='.*' --exclude='*/.*' \
+# sr320@gannet.fish.washington.edu:/volume1/v1_web/owlshell/bu-github/
+
+rsync -av ../project-lake-trout \
+--exclude='*.sam' \
+--exclude='tmp*' \
+--exclude='*gz_C_to_T.fastq' \
+--exclude='*gz_G_to_A.fastq' \
+--exclude='Non_CpG_context*' \
+--exclude='.*' --exclude='*/.*' \
+sr320@gannet.fish.washington.edu:/volume1/v1_web/owlshell/bu-github/
+
+
